84a33f69cd51283005b13ef777df6ad48916bc80
qichengzx <qichengzx@gmail.com>
2020-08-15 16:19:08 +0800

    cmd info

--- server/app.go
+++ server/app.go
@@ -14,11 +14,13 @@ type App struct {
 	db     *raptor.Raptor
 	authed bool
 
-	infoClients infoClients
-}
-
-type infoClients struct {
-	connections int
+	infoClients struct {
+		connections int
+	}
+	infoStat struct {
+		totalConnectionsReceived int
+		totalCommandsProcessed   int
+	}
 }
 
 func New(conf *config.Config) *App {
@@ -71,7 +73,10 @@ func (app *App) onCommand() func(conn redcon.Conn, cmd redcon.Command) {
 			} else {
 				conn.WriteError(ErrPassword)
 			}
+		case "info":
+
 		default:
+			app.infoStat.totalCommandsProcessed++
 			if !app.authed {
 				conn.WriteError(ErrNoAuth)
 				return
@@ -97,6 +102,7 @@ func (app *App) onAccept() func(conn redcon.Conn) bool {
 	return func(conn redcon.Conn) bool {
 		log.Printf("accept: %s", conn.RemoteAddr())
 		app.infoClients.connections++
+		app.infoStat.totalConnectionsReceived++
 		return true
 	}
 }
